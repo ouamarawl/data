6b17c08bbb95bd41e44a8842b8c688e8a427f28b
ouamarawl <ouamara.wail8@gmail.com>
2024-09-27 18:18:30 +0200

    hiiii

--- Index.js
+++ Index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+// hi
 const produits = [
   { id: 1, nom: 'Laptop', prix: 1200 },
   { id: 2, nom: 'Smartphone', prix: 800 },
